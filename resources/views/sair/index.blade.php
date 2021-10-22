864bcac9ac50a16b43e76dbd57ac560ad8957465
André Gomes <andregds85@gmail.com>
2021-10-22 18:59:19 -0300

    teste
    teste

---
+++ resources/views/sair/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+
+<!-- topo !-->
+<div class="card mb-3">
+    <div class="card-body">
+      <h5 class="card-title"><b>Sair</b></h5>
+      <h6 class="card-title"><b></b></h6>
+        <BR>
+    </div>
+  </div>
+
+  <?php
+
+  session()->flush();
+
+
+ ?>
+
+
+
+
+
+
+@endsection
+
+
+
